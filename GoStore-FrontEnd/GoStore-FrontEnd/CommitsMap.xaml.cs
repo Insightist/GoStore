b595bc323c50a7b9a3dd1a5e0505744114257031
Tankle L <Tankle_@hotmail.com>
2016-02-25 20:43:25 +0800

    modified control: CommitsMap. Changed design
      *  change the storage design of the class.
      *  Use track(or you can call it branch) concept. To implement me-
      thod ReDraw().

--- GoStore-FrontEnd/GoStore-FrontEnd/CommitsMap.xaml.cs
+++ GoStore-FrontEnd/GoStore-FrontEnd/CommitsMap.xaml.cs
@@ -38,6 +38,18 @@ namespace GoStore_FrontEnd
             public List<TimeNode>   parentNodes;
         };
 
+        public class Track
+        {
+            public Track()
+            {
+
+            }
+
+
+
+            List<TimeNode> _nodes;
+        };
+
         // Methods:
 
         public CommitsMap()
@@ -49,8 +61,6 @@ namespace GoStore_FrontEnd
 
             _topIndex = 0;
 
-            _nodes = new List<TimeNode>();
-            _startNodeIndecies = new List<uint>();
         }
         
         public void AddNode(TimeNode timeNode)
@@ -58,7 +68,6 @@ namespace GoStore_FrontEnd
             timeNode.index = _topIndex++;
             timeNode.drawn = false;
 
-            _nodes.Add(timeNode);
         }
 
         public bool RemoveNode(uint index)
@@ -68,8 +77,6 @@ namespace GoStore_FrontEnd
 
         public void ClearNodes()
         {
-            _nodes.Clear();
-            _startNodeIndecies.Clear();
             _topIndex = 0;
         }
 
@@ -79,106 +86,10 @@ namespace GoStore_FrontEnd
             canvas.Width = 0;
             canvas.Height = 0;
 
-            foreach(TimeNode eachnode in _nodes)
-            {
-                eachnode.drawn = false;
-            }
-
-            uint depth = 0;
-
-            Stack<TimeNode> undrawNodeStack = new Stack<TimeNode>();
-            Stack<uint> levels = new Stack<uint>();
-            TimeNode currNode;
-            RadioButton radioBtn;
-            Thickness margin = new Thickness();
-            foreach (int startNodeIdx in _startNodeIndecies)
-            {
-                // Point to the start node, and get ready to start drawing loop
-                currNode = _nodes[startNodeIdx];
-                depth = 0;
-
-                // Drawing loop. The tree depth can't exceed maxDepth.
-                while(depth <= maxDepth)
-                {
-                    // Draw current node.
-
-                    margin.Top = (depth) * 50;
-
-                    if(canvas.Height < (depth+1) * 50)
-                    {
-                        canvas.Height = (depth+1) * 50;
-                    }
-
-
-                    radioBtn = new RadioButton();
-                    radioBtn.Content = currNode.name;
-                    radioBtn.Margin = margin;
-
-                    canvas.Children.Add(radioBtn);
-
-
-                    // Judge the loop boundary.
-                    if(currNode.parentNodes.Count == 0 || currNode.drawn == true)
-                    {
-                        if (undrawNodeStack.Count == 0)  // Nothing to draw. Finished.
-                        {
-                            // Finished drawing.
-                            currNode.drawn = true;
-
-                            break;
-                        }
-                        else
-                        {
-                            // Finished drawing.
-                            currNode.drawn = true;
-
-                            // Draw remaining forks.
-                            currNode = undrawNodeStack.Pop();
-                            depth = levels.Pop();
-                            margin.Left += 50;
-                            
-                            if(canvas.Width < margin.Left)
-                            {
-                                canvas.Width = margin.Left;
-                            }
-                            continue;
-                        }
-                    }
-                    // Save the forks.
-                    else if(currNode.parentNodes.Count > 1)
-                    {
-                        for (int i = 1; i < currNode.parentNodes.Count; ++i)
-                        {
-                            if (currNode.parentNodes[i].drawn == false)
-                            {
-                                undrawNodeStack.Push(currNode.parentNodes[i]);
-                                levels.Push(depth + 1);
-                            }
-                        }
-                    }
-
-                    // Point to the left parent node, continue the loop.
-                    currNode.drawn = true;
-                    currNode = currNode.parentNodes[0];
-                    ++depth;
-                }
-
-                margin.Left += 50;
-                if (canvas.Width < margin.Left)
-                {
-                    canvas.Width = margin.Left;
-                }
-            }
-        }
-
-        public void AddStartNodeIndex(uint index)
-        {
-            _startNodeIndecies.Add(index);
         }
         
         // Properties:
-        List<uint>      _startNodeIndecies;
-        List<TimeNode>  _nodes;
+        List<Track>     _tracks;
         uint            _topIndex;
     }
 }
